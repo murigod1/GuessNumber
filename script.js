a2822684d11e56cac451bb6e461aa0a7556488e6
Murilo Jesus <murilexfirebr@gmail.com>
2023-02-20 01:09:46 -0300

    adition of the game logic

--- script.js
+++ script.js
@@ -11,11 +11,38 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
+const secretNuber = Math.trunc(Math.random()*20) +1;
+document.querySelector('.number').textContent = secretNuber;
+let reduceScore = 20;
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
     if(!guess) {
-     document.querySelector('.message').textContent = 'No number ❌'
+     document.querySelector('.message').textContent = 'No number ❌';
     }
-});
+    else if (guess === secretNuber) {
+        document.querySelector('.message').textContent = 'You are correct 🎉';
+    }
+    else if (guess > secretNuber) {
+        if(reduceScore > 1){
+        document.querySelector('.message').textContent = 'Too high 📈';
+        reduceScore = reduceScore - 1;
+        document.querySelector('.score').textContent = reduceScore;
+    } else { 
+        document.querySelector('.message').textContent = '💣 Game Over';
+        document.querySelector('.score').textContent = 0;
+    }
+    }
+    else if (guess < secretNuber) {
+        if(reduceScore > 1){
+            document.querySelector('.message').textContent = 'Too low 📉'
+            reduceScore = reduceScore - 1;
+            document.querySelector('.score').textContent = reduceScore;
+        } else { 
+            document.querySelector('.message').textContent = '💣 Game Over';
+            document.querySelector('.score').textContent = 0;
+        }
+}});
+
